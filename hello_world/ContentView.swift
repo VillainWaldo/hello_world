fe77c47be2c25c60692bab7fba5175a54df40c43
villainwaldo <vslavov150@gmail.com>
2022-02-03 13:48:16 -0500

    Initial Commit

---
+++ hello_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello_world
+//
+//  Created by Vladimir Slavov on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Vladimir Slavov on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
